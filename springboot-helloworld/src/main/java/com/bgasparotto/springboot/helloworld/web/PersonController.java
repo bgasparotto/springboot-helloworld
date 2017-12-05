4f0025d6dbcf3959cc2b7ed5331a3326193423ab
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-12-05 14:54:57 -0200

    Added missing javadocs.

--- springboot-helloworld/src/main/java/com/bgasparotto/springboot/helloworld/web/PersonController.java
+++ springboot-helloworld/src/main/java/com/bgasparotto/springboot/helloworld/web/PersonController.java
@@ -10,13 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bgasparotto.springboot.helloworld.model.Person;
 import com.bgasparotto.springboot.helloworld.service.PersonService;
 
+/**
+ * REST controller for the {@code Person} resource.
+ * 
+ * @author Bruno Gasparotto
+ *
+ */
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
-	
+
 	@Autowired
 	private PersonService service;
-	
+
+	/**
+	 * List all the available {@code Person} resources in the system.
+	 * 
+	 * @return Collection of {@code Person}
+	 */
 	@GetMapping
 	public List<Person> findAll() {
 		return service.findAll();
